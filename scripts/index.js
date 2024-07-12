72a2798ebee12466dacf734d041a2856b276f865
DravenTheRaven <106798464+DravenTheRaven@users.noreply.github.com>
2024-07-11 23:35:27 -0500

    added js functionality. the cards are now rendered by js via a template. the modal edit window now edits the profile information.

--- scripts/index.js
+++ scripts/index.js
@@ -25,14 +25,46 @@ const initialCards = [
   },
 ];
 
+let template = document.querySelector("#card__template");
+let cardContainer = document.querySelector(".cards");
 let editButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".modal__close-button");
 let modal = document.querySelector(".modal");
-console.log(closeButton);
+let formName = document.querySelector(".profile__title");
+let formDescription = document.querySelector(".profile__subtitle");
+let formButton = document.querySelector(".form__button");
+let newFormName = document.querySelector(".form__name");
+let newFormDescription = document.querySelector(".form__description");
 
-function toggleModalDisplay() {
+function openModalDisplay(event) {
+  newFormDescription.value = `${formDescription.textContent}`;
+  newFormName.value = `${formName.textContent}`;
   modal.classList.toggle("modal_opened");
 }
 
-editButton.addEventListener("click", toggleModalDisplay);
-closeButton.addEventListener("click", toggleModalDisplay);
+function closeModalDisplay(event) {
+  modal.classList.toggle("modal_opened");
+}
+
+function changeProfileInfo(event) {
+  formName.textContent = `${newFormName.value}`;
+  formDescription.textContent = `${newFormDescription.value}`;
+  modal.classList.toggle("modal_opened");
+  event.preventDefault();
+}
+
+editButton.addEventListener("click", openModalDisplay);
+closeButton.addEventListener("click", closeModalDisplay);
+formButton.addEventListener("click", changeProfileInfo);
+
+function getCardElement(data) {
+  for (let x of data) {
+    let templateClone = template.content.cloneNode(true);
+    templateClone.querySelector(".card__image").src = x.link;
+    templateClone.querySelector(".card__image").alt = x.name;
+    templateClone.querySelector(".card__text").textContent = x.name;
+    cardContainer.append(templateClone);
+  }
+}
+
+getCardElement(initialCards);
