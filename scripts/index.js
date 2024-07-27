9f6717737c0f109bf8d74301ebce7191580df5e4
DravenTheRaven <106798464+DravenTheRaven@users.noreply.github.com>
2024-07-27 00:48:37 -0500

    sprint 5 corrections round 1

--- scripts/index.js
+++ scripts/index.js
@@ -28,55 +28,39 @@ const initialCards = [
 const template = document.querySelector("#card__template");
 const cardContainer = document.querySelector(".cards");
 const editButton = document.querySelector(".profile__edit-button");
-const closeButton = document.querySelector(".edit-modal__close-button");
-const addCloseButton = document.querySelector(".add-modal__close-button");
-const imageCloseButton = document.querySelector(".modal-image__close-button");
+
 const addButton = document.querySelector(".profile__button");
 const imageModal = document.querySelector(".image-modal");
-const formName = document.querySelector(".profile__title");
-const formDescription = document.querySelector(".profile__subtitle");
+const profileFormName = document.querySelector(".profile__title");
+const profileFormDescription = document.querySelector(".profile__subtitle");
 const editForm = document.querySelector(".edit-form");
 const addForm = document.querySelector(".add-form");
-const newFormName = document.querySelector(".form__name");
-const newFormDescription = document.querySelector(".form__description");
+const newProfileFormName = document.querySelector(".form__name");
+const newProfileFormDescription = document.querySelector(".form__description");
+const editModal = document.querySelector(".edit-modal");
+const addModal = document.querySelector(".add-modal");
+const closeButtons = document.querySelectorAll(".modal__close-button");
 
-function toggleProfileModal(event) {
-  const editModal = document.querySelector(".edit-modal");
-  const addModal = document.querySelector(".add-modal");
-  if (
-    event.target.classList[0] === "profile__edit-button" ||
-    event.target.classList[1] === "edit-modal__close-button" ||
-    event.target.classList[1] === "edit-form"
-  ) {
-    editModal.classList.toggle("modal_opened");
-  } else if (
-    event.target.classList[0] === "profile__button" ||
-    event.target.classList[1] === "add-modal__close-button" ||
-    event.target.classList[1] === "add-form"
-  ) {
-    addModal.classList.toggle("modal_opened");
-  } else if (
-    event.target.classList[0] === "card__image" ||
-    event.target.classList[1] === "modal-image__close-button"
-  ) {
-    imageModal.classList.toggle("modal_opened");
-  }
+function openAndCloseModal(modal) {
+  modal.classList.toggle("modal_opened");
 }
 
-function openModalDisplay(event) {
-  newFormDescription.value = `${formDescription.textContent}`;
-  newFormName.value = `${formName.textContent}`;
-  toggleProfileModal(event);
+function openProfileModal(event) {
+  newProfileFormDescription.value = `${profileFormDescription.textContent}`;
+  newProfileFormName.value = `${profileFormName.textContent}`;
+  openAndCloseModal(editModal);
 }
 
-function closeModalDisplay(event) {
-  toggleProfileModal(event);
+function openAddModal() {
+  document.querySelector("#title").value = "";
+  document.querySelector(".form__image-url").value = "";
+  openAndCloseModal(addModal);
 }
 
 function changeProfileInfo(event) {
-  formName.textContent = `${newFormName.value}`;
-  formDescription.textContent = `${newFormDescription.value}`;
-  toggleProfileModal(event);
+  profileFormName.textContent = `${newProfileFormName.value}`;
+  profileFormDescription.textContent = `${newProfileFormDescription.value}`;
+  openAndCloseModal(editModal);
   event.preventDefault();
 }
 
@@ -84,54 +68,55 @@ function addLocationCard(event) {
   const newLocationTitle = document.querySelector("#title").value;
   const newLocationImage = document.querySelector(".form__image-url").value;
   const newLocation = { name: newLocationTitle, link: newLocationImage };
-  cardContainer.append(createCard(newLocation));
-  toggleProfileModal(event);
+  cardContainer.prepend(createCard(newLocation));
+  openAndCloseModal(addModal);
   event.preventDefault();
 }
 
-function likeButton(event) {
+function likeCard(event) {
   event.target.classList.toggle("card__button-clicked");
 }
 
-function deleteButton(event) {
-  event.target.parentElement.remove();
+function deleteCard(event) {
+  event.target.closest(".card").remove();
 }
 
 function openImageModel(event) {
-  imageModal.querySelector(".modal__image").src = event.target.src;
-  imageModal.querySelector(".modal__image").alt = event.target.alt;
-  imageModal.querySelector(".modal-image__text").textContent = event.target.alt;
-  console.log(imageModal.querySelector(".modal-image__text").textContent);
-  toggleProfileModal(event);
+  imageModal.querySelector(".image-modal__image").src = event.target.src;
+  imageModal.querySelector(".image-modal__image").alt = event.target.alt;
+  imageModal.querySelector(".image-modal__text").textContent = event.target.alt;
+  openAndCloseModal(imageModal);
 }
 
-editButton.addEventListener("click", openModalDisplay);
-addButton.addEventListener("click", openModalDisplay);
-closeButton.addEventListener("click", closeModalDisplay);
-addCloseButton.addEventListener("click", closeModalDisplay);
-imageCloseButton.addEventListener("click", closeModalDisplay);
+editButton.addEventListener("click", openProfileModal);
+addButton.addEventListener("click", openAddModal);
 editForm.addEventListener("submit", changeProfileInfo);
 addForm.addEventListener("submit", addLocationCard);
+closeButtons.forEach((button) => {
+  const modal = button.closest(".modal");
+  button.addEventListener("click", () => openAndCloseModal(modal));
+});
 
 function createCard(cardData) {
   const templateClone = template.content.cloneNode(true);
-  templateClone.querySelector(".card__image").src = cardData.link;
-  templateClone.querySelector(".card__image").alt = cardData.name;
+  const cardImage = templateClone.querySelector(".card__image");
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
   templateClone.querySelector(".card__text").textContent = cardData.name;
   templateClone
     .querySelector(".card__button")
-    .addEventListener("click", likeButton);
+    .addEventListener("click", likeCard);
   templateClone
     .querySelector(".card__delete-button")
-    .addEventListener("click", deleteButton);
+    .addEventListener("click", deleteCard);
   templateClone
     .querySelector(".card__image")
     .addEventListener("click", openImageModel);
   return templateClone;
 }
 
-function renderCard(data) {
+function renderCards(data) {
   data.forEach((element) => cardContainer.append(createCard(element)));
 }
 
-renderCard(initialCards);
+renderCards(initialCards);
